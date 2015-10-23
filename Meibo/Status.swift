2d7a8e3e7ae4095083115e9870ad994822a6e5a0
Boris <zhouboli@gmail.com>
2015-10-24 00:26:29 +0800

    finished one model only

--- Meibo/Status.swift
+++ Meibo/Status.swift
@@ -31,71 +31,84 @@ class Status: NSObject {
     
     func initWithDictionary(dictionary: NSDictionary) -> Status
     {
-        if dictionary.objectForKey("created_at")
+        if (dictionary.objectForKey("created_at") != nil)
         {
             self.created_at = dictionary.objectForKey("created_at") as? String
         }
-        if dictionary.objectForKey("id")
+        if (dictionary.objectForKey("id") != nil)
         {
             self.status_id = Int(dictionary.objectForKey("id")! as! String)
         }
-        if dictionary.objectForKey("mid")
+        if (dictionary.objectForKey("mid") != nil)
         {
             self.status_mid = Int(dictionary.objectForKey("mid")! as! String)
         }
-        if dictionary.objectForKey("idstr")
+        if (dictionary.objectForKey("idstr") != nil)
         {
             self.idstr = dictionary.objectForKey("idstr") as? String
         }
-        if dictionary.objectForKey("text")
+        if (dictionary.objectForKey("text") != nil)
         {
             self.text = dictionary.objectForKey("text") as? String
         }
-        if dictionary.objectForKey("source")
+        if (dictionary.objectForKey("source") != nil)
         {
             self.source = dictionary.objectForKey("source") as? String
         }
-        if dictionary.objectForKey("favorited")
+        if (dictionary.objectForKey("favorited") != nil)
         {
             self.favorited = (dictionary.objectForKey("favorited") as? Bool)!
         }
-        if dictionary.objectForKey("truncated")
+        if (dictionary.objectForKey("truncated") != nil)
         {
             self.truncated = (dictionary.objectForKey("truncated") as? Bool)!
         }
-        if dictionary.objectForKey("thumbnail_pic")
+        if (dictionary.objectForKey("thumbnail_pic") != nil)
         {
             self.thumbnail_pic = dictionary.objectForKey("thumbnail_pic") as? String
         }
-        if dictionary.objectForKey("bmiddle_pic")
+        if (dictionary.objectForKey("bmiddle_pic") != nil)
         {
             self.bmiddle_pic = dictionary.objectForKey("bmiddle_pic") as? String
         }
-        if dictionary.objectForKey("original_pic")
+        if (dictionary.objectForKey("original_pic") != nil)
         {
             self.original_pic = dictionary.objectForKey("original_pic") as? String
         }
-        if dictionary.objectForKey("reposts_count")
+        if (dictionary.objectForKey("reposts_count") != nil)
         {
             self.reposts_count = Int(dictionary.objectForKey("reposts_count")! as! String)
         }
-        if dictionary.objectForKey("comments_count")
+        if (dictionary.objectForKey("comments_count") != nil)
         {
             self.comments_count = Int(dictionary.objectForKey("comments_count")! as! String)
         }
-        if dictionary.objectForKey("attitudes_count")
+        if (dictionary.objectForKey("attitudes_count") != nil)
         {
             self.attitudes_count = Int(dictionary.objectForKey("attitudes_count")! as! String)
         }
-        if dictionary.objectForKey("pic_urls")
+        if (dictionary.objectForKey("pic_urls") != nil)
         {
+            let urls: NSArray = (dictionary.objectForKey("pic_urls") as? NSArray)!
             self.pic_urls = NSMutableArray(capacity: 9)
-            for _: String in self.pic_urls!
+            for dict in urls
             {
-                self.pic_urls.addObject(_)
+                self.pic_urls?.addObject(dict.objectForKey("thumbnail_pic")!)
             }
         }
+        if dictionary.objectForKey("user") != nil
+        {
+            let dict = dictionary.objectForKey("user") as? NSDictionary
+            self.user = User().initWithDictionary(dict!)
+        }
+        if dictionary.objectForKey("retweeted_status") != nil
+        {
+            self.retweeted_status = Status().initWithDictionary((dictionary.objectForKey("retweeted_status") as? NSDictionary)!)
+        }
         
+        
+        
+        return self
     }
 
 
