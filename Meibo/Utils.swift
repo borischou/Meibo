3f2c3a0a9e89b09b2697fe806bcfefc174c39520
Boris <zhouboli@gmail.com>
2015-10-26 00:06:18 +0800

    moving faster

--- Meibo/Utils.swift
+++ Meibo/Utils.swift
@@ -9,34 +9,118 @@
 import Foundation
 import UIKit
 
-let uBigGap = 5
-let uSmallGap = 10
-let uHeadImgHeight = 50
+let uBigGap: CGFloat = 5
+let uSmallGap: CGFloat = 10
+let uHeadImgHeight: CGFloat = 50
+let sWidth: CGFloat = UIScreen.mainScreen().bounds.size.width
+let sHeight: CGFloat = UIScreen.mainScreen().bounds.size.height
+let imageHeight = (sWidth - 2 * uBigGap)/3
+let imageHeightForTwo = (sWidth - uBigGap)/2
 
 class Utils: NSObject {
     
-    static func fontForStatus() -> Float
+    static func fontForStatus() -> CGFloat
     {
         return 16.0
     }
     
-    static func height(string: String, width: Float, fontSize: Float) -> Float
+    static func height(str: NSString, width: CGFloat, fontSize: CGFloat) -> CGFloat
     {
-        if string.isEmpty
+        if str.length == 0
         {
             return 0
         }
-        var parastyle: NSMutableParagraphStyle = NSMutableParagraphStyle()
+        let parastyle: NSMutableParagraphStyle = NSMutableParagraphStyle()
         parastyle.lineBreakMode = .ByWordWrapping
         parastyle.lineSpacing = 2.0
-        var strsize: CGSize = 
+        let strRect: CGRect = str.boundingRectWithSize(CGSizeMake(width, 5000), options: [.UsesLineFragmentOrigin, .UsesFontLeading], attributes: [NSFontAttributeName: UIFont.systemFontOfSize(fontSize),
+            NSParagraphStyleAttributeName: parastyle], context: nil)
+        let strSize: CGSize = strRect.size
+        return strSize.height
     }
     
-    static func getHeightForCell(text: String, statusImageNumber: NSInteger, retweetScreenName: String, retweetText: String, retweetImageNumber: NSInteger) -> Float
+    static func heightForImages(count: NSInteger) -> CGFloat
     {
-        var height: Float = 0
-        var fontSize = Utils.fontForStatus()
-        height += uBigGap+uHeadImgHeight
+        var height: CGFloat = 0
         
+        if count == 1 || count == 2
+        {
+            height = imageHeightForTwo
+        }
+        if count == 3
+        {
+            height = imageHeight
+        }
+        if count == 4
+        {
+            height = imageHeightForTwo * 2 + uSmallGap
+        }
+        if count > 4 && count <= 6
+        {
+            height = imageHeight * 2 + uSmallGap
+        }
+        if count >= 7 && count <= 9
+        {
+            height = imageHeight * 3 + uSmallGap * 2
+        }
+        
+        return height
     }
+    
+    static func getHeightForCell(text: String, statusImageNumber: NSInteger, retweetScreenName: String, retweetText: String, retweetImageNumber: NSInteger) -> CGFloat
+    {
+        var height: CGFloat = 0
+        let fontSize = Utils.fontForStatus()
+        
+        //头像
+        height += uBigGap + uHeadImgHeight
+        
+        //微博文本
+        height += uBigGap + Utils.height(text, width: sWidth - uBigGap * 2, fontSize: fontSize)
+        
+        //微博配图
+        if statusImageNumber > 0
+        {
+            height += uSmallGap + Utils.heightForImages(statusImageNumber)
+        }
+        
+        
+        
+        return height
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
 }
