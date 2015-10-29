c07875b8f1709609700fc4d9baa8f2252de6c74f
Boris <zhouboli@gmail.com>
2015-10-29 17:51:35 +0800

    added feedback model

---
+++ Meibo/Feedback.swift
@@ -0,0 +1,27 @@
+//
+//  Feedback.swift
+//  Meibo
+//
+//  Created by Zhouboli on 15/10/29.
+//  Copyright © 2015年 Boris. All rights reserved.
+//
+
+import Foundation
+
+class Feedback: NSObject {
+    var
+    status_id: NSInteger?,
+    comments: NSInteger?,
+    reposts: NSInteger?,
+    attitudes: NSInteger?
+    
+    func initWithNSDictionary(dictionary: NSDictionary) -> Feedback
+    {
+        self.status_id = (dictionary.objectForKey("id") as? NSString)?.integerValue
+        self.comments = (dictionary.objectForKey("comments") as? NSString)?.integerValue
+        self.reposts = (dictionary.objectForKey("reposts") as? NSString)?.integerValue
+        self.attitudes = (dictionary.objectForKey("attitudes") as? NSString)?.integerValue
+        
+        return self
+    }
+}
